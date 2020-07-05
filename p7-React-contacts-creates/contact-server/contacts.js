8423c8e813bbe7fce376070629c3717749ee4a7c
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-05 23:08:19 +0530

    add contact server

---
+++ p7-React-contacts-creates/contact-server/contacts.js
@@ -0,0 +1,64 @@
+const clone = require('clone')
+const config = require('./config')
+
+const db = {}
+
+const defaultData = {
+  contacts: [
+    {
+      id: 'richard',
+      name: 'Richard Kalehoff',
+      handle: '@richardkalehoff',
+      avatarURL: config.origin + '/richard.jpg'
+    },
+    {
+      id: 'karen',
+      name: 'Karen Isgrigg',
+      handle: '@karen_isgrigg',
+      avatarURL: config.origin + '/karen.jpg'
+    },
+    {
+      id: 'tyler',
+      name: 'Tyler McGinnis',
+      handle: '@tylermcginnis',
+      avatarURL: config.origin + '/tyler.jpg'
+    }
+  ]
+}
+
+const get = (token) => {
+  let data = db[token]
+
+  if (data == null) {
+    data = db[token] = clone(defaultData)
+  }
+
+  return data
+}
+
+const add = (token, contact) => {
+  if (!contact.id) {
+    contact.id = Math.random().toString(36).substr(-8)
+  }
+
+  get(token).contacts.push(contact)
+
+  return contact
+}
+
+const remove = (token, id) => {
+  const data = get(token)
+  const contact = data.contacts.find(c => c.id === id)
+
+  if (contact) {
+    data.contacts = data.contacts.filter(c => c !== contact)
+  }
+
+  return { contact }
+}
+
+module.exports = {
+  get,
+  add,
+  remove
+}
