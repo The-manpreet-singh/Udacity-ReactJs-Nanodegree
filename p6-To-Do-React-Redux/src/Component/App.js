90e7a5458e4101ab9a4924363045e19d927da7e1
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-05 22:59:45 +0530

    add components

---
+++ p6-To-Do-React-Redux/src/Component/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ConnectedTodos from "./Todos";
+import ConnectedGoals from "./Goals";
+import { connect } from "react-redux";
+import { handleInitialData } from "../Actions/shared";
+
+class App extends React.Component {
+	componentDidMount() {
+		const { dispatch } = this.props;
+
+		dispatch(handleInitialData());
+	}
+	render() {
+		if (this.props.loading === true) {
+			return <h3>Loading</h3>;
+		}
+
+		return (
+			<div>
+				<ConnectedTodos />
+				<ConnectedGoals />
+			</div>
+		);
+	}
+}
+
+export default connect((state) => ({
+	loading: state.loading,
+}))(App);
