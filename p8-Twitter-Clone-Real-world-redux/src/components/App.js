b1e0b774882e1a4147196a7a81c5376d6fd03686
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-06 11:01:34 +0530

    add app.js

---
+++ p8-Twitter-Clone-Real-world-redux/src/components/App.js
@@ -0,0 +1,42 @@
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
+import LoadingBar from "react-redux-loading";
+import NewTweet from "./NewTweet";
+import TweetPage from "./TweetPage";
+import Nav from "./Nav";
+
+class App extends Component {
+	componentDidMount() {
+		this.props.dispatch(handleInitialData());
+	}
+	render() {
+		return (
+			<Router>
+				<Fragment>
+					<LoadingBar />
+					<div className="container">
+						<Nav />
+						{this.props.loading === true ? null : (
+							<div>
+								<Route path="/" exact component={Dashboard} />
+								<Route path="/tweet/:id" component={TweetPage} />
+								<Route path="/new" component={NewTweet} />
+							</div>
+						)}
+					</div>
+				</Fragment>
+			</Router>
+		);
+	}
+}
+
+function mapStateToProps({ authedUser }) {
+	return {
+		loading: authedUser === null,
+	};
+}
+
+export default connect(mapStateToProps)(App);
