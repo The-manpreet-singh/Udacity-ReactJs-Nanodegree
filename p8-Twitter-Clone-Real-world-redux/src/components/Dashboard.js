3526022edae04c99d288765c71d3371901fc729c
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-06 11:02:57 +0530

    add dashboar.js

---
+++ p8-Twitter-Clone-Real-world-redux/src/components/Dashboard.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Tweet from "./Tweet";
+
+class Dashboard extends Component {
+	render() {
+		return (
+			<div>
+				<h3 className="center">Your Timeline</h3>
+				<ul className="dashboard-list">
+					{this.props.tweetIds.map((id) => (
+						<li key={id}>
+							<Tweet id={id} />
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
+}
+
+function mapStateToProps({ tweets }) {
+	return {
+		tweetIds: Object.keys(tweets).sort((a, b) => tweets[b].timestamp - tweets[a].timestamp),
+	};
+}
+
+export default connect(mapStateToProps)(Dashboard);
