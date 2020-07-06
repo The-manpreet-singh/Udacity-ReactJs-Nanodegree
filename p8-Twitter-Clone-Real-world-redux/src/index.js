baff4b5f850c23a8471fcff99eee6e5e31abd29a
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-06 10:54:52 +0530

    add index.js

---
+++ p8-Twitter-Clone-Real-world-redux/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+
+import middleware from './middleware'
+
+
+const store = createStore(reducer, middleware)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+document.getElementById('root')
+) 
