cb11634250a2194759a5806d8da11f134a3ed8e8
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-08 20:33:43 +0530

    add DateHeader

---
+++ p9-UdaciFitness-Quiz-game/components/DateHeader.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Text } from "react-native";
+import { purple } from "../utils/colors";
+
+export default function DateHeader({ date }) {
+	return <Text style={{ color: purple, fontSize: 25 }}>{date}</Text>;
+}
